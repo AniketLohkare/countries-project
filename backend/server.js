987b7a384f4ed9a1c1c4200948a7442b00dea091
Aniket Lohkare <aniketlohkare6@gmail.com>
2026-06-21 19:40:17 +0530

    Configure port for Render

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
   res.send('Countries API is running')
 })
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
