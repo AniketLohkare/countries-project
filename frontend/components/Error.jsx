b859c97e7d39edc340460fd97e9ac1e14022248e
Aniket Lohkare <aniketlohkare6@gmail.com>
2026-09-18 21:24:22 +0530

    Remove unused contact page

--- frontend/components/Error.jsx
+++ frontend/components/Error.jsx
@@ -3,6 +3,5 @@ import { useRouteError } from 'react-router-dom'
 
 export default function Error() {
   const error = useRouteError()
-  console.log(error);
   return <div>Something went wrong. {error.status}</div>
 }
